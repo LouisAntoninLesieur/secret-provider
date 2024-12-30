04a19bfb12b9cb4602a6f06288f4649338deec47
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-12-30 17:21:48 +0100

    feat: build example page

---
+++ app/cv/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const cv = () => {
+  return (
+    <>
+    <Navbar />
+      <div>cv</div>
+    </>
+  )
+}
+
+export default cv
