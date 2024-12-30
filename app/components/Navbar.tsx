860ab2cb5281a9484f40fa105597e01845d8068e
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-12-30 17:18:43 +0100

    feat: create navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex gap-10 text-4xl font-bold justify-center">
+      <Link href='/'>Home</Link>
+      <Link href='/contact'>Contact</Link>
+      <Link href='/cv'>CV</Link>
+      <Link href='/jeu'>Jeu</Link>
+    </nav>
+  );
+}
