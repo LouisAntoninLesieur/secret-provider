ccf95da4cfdfd377cbab093d79a26d79adf8fda1
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-01 23:33:17 +0100

    refactor: edit home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,18 @@
 "use client"
 
-import Navbar from "./components/Navbar";
-import HomeListItem from "./components/HomeListItem";
+// import Navbar from "./components/Navbar";
+// import HomeListItem from "./components/HomeListItem";
 
 export default function Home() {
 
-  const els = [
-    {title: "Home", description: "Home description", link: "/"}, 
-    {title: "Contact", description: "Contact description", link: "/contact"}, 
-    {title: "CV", description: "CV description", link: "/cv"}, 
-    {title: "Game", description: "Game description", link: "/game"}]
+  // const els = [
+  //   {title: "Generate secret", description: "Generate secret description", link: "/generate-secret"}, 
+  //   {title: "Contact", description: "Contact description", link: "/contact"}, 
+  // ]
 
   return (
     <>
-      <Navbar />
-
-      <ul className="flex flex-col gap-4">
-        {els.map((el) => {
-          return <HomeListItem key={el.title} title={el.title} description={el.description} link={el.link} />
-        })}
-      </ul>
+    
     </>
   );
 }
