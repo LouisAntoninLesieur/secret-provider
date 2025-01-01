790774a83bf11caa4b5b326791183675a02b6b67
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-01 23:40:19 +0100

    feat: mount Secret Form Component

---
+++ app/components/SecretForm.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useState, ChangeEvent, FormEvent } from 'react';
+import InputField from './Form/InputField';
+import GenerateButton from './Form/GenerateButton';
+import SecretDisplay from './Form/SecretDisplay';
+import generateSecret from '@/utils/generateSecret';
+
+const SecretForm = () => {
+  const [secret, setSecret] = useState('');
+  const [requiredBytes, setRequiredBytes] = useState(32);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    if (!e.target.value) {
+      setRequiredBytes(0);
+      return;
+    }
+
+    if (parseInt(e.target.value) < 0) {
+      setRequiredBytes(0);
+      return;
+    }
+
+    setRequiredBytes(parseInt(e.target.value));
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const newSecret = generateSecret(requiredBytes);
+    setSecret(newSecret);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='flex flex-col items-center gap-2 border border-black p-4 rounded mx-20'>
+      <InputField value={requiredBytes} onChange={handleChange} />
+      <GenerateButton />
+      <SecretDisplay secret={secret} />
+    </form>
+  );
+};
+
+export default SecretForm;
