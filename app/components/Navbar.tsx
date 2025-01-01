84d8832e2a906c9e9a0d5f2c807525413f73ba24
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-01 23:36:53 +0100

    feat: swhitch link position

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,11 +2,10 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex gap-10 text-4xl font-bold justify-center">
+    <nav className="flex gap-10 text-2xl font-bold justify-center bg-slate-200 p-4 border rounded mx-10 my-10">
       <Link href='/'>Home</Link>
+      <Link href='/generate-secret'>Generate secret</Link>
       <Link href='/contact'>Contact</Link>
-      <Link href='/cv'>CV</Link>
-      <Link href='/game'>Game</Link>
     </nav>
   );
 }
