02bc4bc160f59e717f973d76415212eadc8907b5
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-02 18:37:34 +0100

    feat: change text

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <section className="flex flex-col items-center">
       <div className='flex flex-col items-center p-4 mx-auto mt-20 rounded'>
         <h2 className='text-5xl text-transparent bg-clip-text bg-gradient-to-br from-slate-00 via-slate-100 to-slate-50 text-center font-bold'>JWT Secret Generator</h2>
-        <p className='text-lg'>Generate a secret. We promise we won't tell.</p>
+        <p className='text-lg'>Generate a secret. We promise nobody will know.</p>
         <hr className='w-full my-4' />
       <HomeListItem title="Generate a secret now" link="/generate-secret" />
       </div>
