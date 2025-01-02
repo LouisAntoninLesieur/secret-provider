e2de2dcdee04252a7980a01acbaa72822944be84
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-02 18:18:41 +0100

    feat: modify home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
-"use client"
-
-// import Navbar from "./components/Navbar";
-// import HomeListItem from "./components/HomeListItem";
+import HomeListItem from "./components/HomeListItem";
 
 export default function Home() {
 
-  // const els = [
-  //   {title: "Generate secret", description: "Generate secret description", link: "/generate-secret"}, 
-  //   {title: "Contact", description: "Contact description", link: "/contact"}, 
-  // ]
-
   return (
-    <>
-    
-    </>
+    <section className="flex flex-col items-center">
+      <div className='flex flex-col items-center p-4 mx-auto mt-20 rounded'>
+        <h2 className='text-5xl text-transparent bg-clip-text bg-gradient-to-br from-slate-00 via-slate-100 to-slate-50 text-center font-bold'>JWT Secret Generator</h2>
+        <p className='text-lg'>Generate a secret. We promise we won't tell.</p>
+        <hr className='w-full my-4' />
+      <HomeListItem title="Generate a secret now" link="/generate-secret" />
+      </div>
+    </section>
   );
+
 }
