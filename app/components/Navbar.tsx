2f887a5f4083256ef851b9fd49582a7ae015a9e7
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-02 01:03:34 +0100

    refactor(css): modify styles

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex gap-10 text-2xl font-bold justify-center bg-slate-200 p-4 border rounded mx-10 my-10">
+    <nav className="flex gap-10 text-2xl font-bold justify-center text-white bg-gray-200 bg-opacity-20 p-4 border rounded mx-10 my-10">
       <Link href='/'>Home</Link>
       <Link href='/generate-secret'>Generate secret</Link>
       <Link href='/contact'>Contact</Link>
