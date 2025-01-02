eaaf94aa96a5cdffaf76fd9699251b28c7f4d57a
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-02 01:03:19 +0100

    refactor(css): modify styles

--- app/components/Form/InputField.tsx
+++ app/components/Form/InputField.tsx
@@ -1,8 +1,8 @@
 import { ChangeEvent } from "react";
 
 const InputField = ({ value, onChange }: { value: number, onChange: (e: ChangeEvent<HTMLInputElement>) => void }) => (
-  <div className='flex items-center border border-black p-2 rounded'>
-    <input type="number" placeholder="Number of bytes" value={value} onChange={onChange} className='mr-2 w-16' />
+  <div className='flex items-center border border-white p-2 rounded'>
+    <input type="number" placeholder="Number of bytes" value={value} onChange={onChange} className='mr-2 w-16 bg-transparent' />
   </div>
 );
 
