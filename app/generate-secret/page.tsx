7561f3497773fb8eeb442861295a241d61aeeee1
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-02 01:03:48 +0100

    refactor(css): modify styles

--- app/generate-secret/page.tsx
+++ app/generate-secret/page.tsx
@@ -4,7 +4,9 @@ import SecretForm from '../components/SecretForm';
 const GenerateSecretPage = () => {
   return (
     <>
-      <div className="text-2xl font-bold text-center">Generate a random secret, given a number of bytes</div>
+      <div className="text-6xl font-bold text-center w-1/3 mx-auto bg-clip-text text-transparent bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] from-neutral-600 via-green-50 to-green-50 m-4">
+        Generate a random secret, given a number of bytes
+      </div>
       <SecretForm />
     </>
   );
