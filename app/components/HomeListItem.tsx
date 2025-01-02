9391d80675c73ba94dddd6fa93ba83116f9a00e1
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-02 18:22:40 +0100

    feat: modify component

--- app/components/HomeListItem.tsx
+++ app/components/HomeListItem.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import { HomeListItemProps } from '../types/interfaces'
 
-const HomeListItem = ({title, description, link}: {title: string, description: string, link: string}) => {
+const HomeListItem = ({title, link}: HomeListItemProps) => {
   return (
-    <li key={title}>
+    <li key={title} className="flex flex-col items-center">
       <Link href={link}>
-        <div className="flex flex-col p-2 border border-black rounded my-2 mx-auto">
+        <div className="flex flex-col p-2 text-center rounded my-2 mx-auto bg-slate-300 hover:bg-green-700 cursor-pointer">
           <div className="font-bold">{title}</div>
-          <div className='italic'>{description}</div>
         </div>
       </Link>
     </li>
