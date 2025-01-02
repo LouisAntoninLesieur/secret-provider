45c7ca2774cf8e0145bac9b69bfcef1fb8604831
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-02 18:23:03 +0100

    feat: modify component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,10 +2,8 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex gap-10 text-2xl font-bold justify-center text-white bg-gray-200 bg-opacity-20 p-4 border rounded mx-10 my-10">
-      <Link href='/'>Home</Link>
-      <Link href='/generate-secret'>Generate secret</Link>
-      <Link href='/contact'>Contact</Link>
+    <nav className="flex gap-10 text-2xl font-bold justify-start text-white bg-gray-200 bg-opacity-20 p-4 border rounded mx-10 my-10">
+      <Link href='/'>SecretProvider.com</Link>
     </nav>
   );
 }
