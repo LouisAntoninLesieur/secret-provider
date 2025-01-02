09660d4a6ef3666e05a4d3542116e860c46db4f3
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-02 01:03:07 +0100

    refactor(css): modify styles

--- app/components/Form/GenerateButton.tsx
+++ app/components/Form/GenerateButton.tsx
@@ -1,5 +1,5 @@
 const GenerateButton = () => (
-  <button type="submit" className='bg-green-500 text-white px-4 py-2'>Generate Secret</button>
+  <button type="submit" className='bg-green-500 rounded text-white px-4 py-2'>Generate Secret</button>
 );
 
 export default GenerateButton
