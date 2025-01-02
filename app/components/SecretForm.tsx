b8e18ffde07ef264a57a07f809759765109e9288
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-02 18:23:39 +0100

    feat: major component change

--- app/components/SecretForm.tsx
+++ app/components/SecretForm.tsx
@@ -1,42 +1,47 @@
 'use client'
 
-import { useState, ChangeEvent, FormEvent } from 'react';
+import { useState, useEffect, ChangeEvent, FormEvent } from 'react';
 import InputField from './Form/InputField';
 import GenerateButton from './Form/GenerateButton';
+import CopyButton from './Form/CopyButton';
 import SecretDisplay from './Form/SecretDisplay';
 import generateSecret from '@/utils/generateSecret';
 
 const SecretForm = () => {
+  const values = [32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512];
+  const chrono = 8000;
   const [secret, setSecret] = useState('');
-  const [requiredBytes, setRequiredBytes] = useState(32);
+  const [requiredBytes, setRequiredBytes] = useState(256);
+  const [timeLeft, setTimeLeft] = useState(0);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
     e.preventDefault();
-
-    if (!e.target.value) {
-      setRequiredBytes(0);
-      return;
-    }
-
-    if (parseInt(e.target.value) < 0) {
-      setRequiredBytes(0);
-      return;
-    }
-
     setRequiredBytes(parseInt(e.target.value));
   };
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    const newSecret = generateSecret(requiredBytes);
-    setSecret(newSecret);
+    setSecret(generateSecret({ bytes: requiredBytes }));
+    setTimeLeft(chrono / 1000);
+    setTimeout(() => setSecret(''), chrono);
   };
 
+  useEffect(() => {
+    if (timeLeft > 0) {
+      const interval = setInterval(() => {
+        setTimeLeft(prevTime => prevTime - 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [timeLeft]);
+
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col items-center gap-2 border border-white p-4 text-white bg-gray-200 bg-opacity-20 rounded mx-20'>
-      <InputField value={requiredBytes} onChange={handleChange} />
+    <form onSubmit={handleSubmit} className='flex flex-col w-screen items-center gap-2'>
+      <InputField array={values} onChange={handleChange} />
       <GenerateButton />
+      {secret && <p className='font-bold'>This secret is going to expire in {timeLeft} seconds</p>}
       <SecretDisplay secret={secret} />
+      <CopyButton secret={secret}/>
     </form>
   );
 };
