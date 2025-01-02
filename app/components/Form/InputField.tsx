2d2c20158d1aaff1dc939c22d20042dd431122ab
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-02 18:21:34 +0100

    refactor(component): change from input to a list giving definded values

--- app/components/Form/InputField.tsx
+++ app/components/Form/InputField.tsx
@@ -1,9 +1,20 @@
-import { ChangeEvent } from "react";
+import { InputFieldProps } from "@/app/types/interfaces";
 
-const InputField = ({ value, onChange }: { value: number, onChange: (e: ChangeEvent<HTMLInputElement>) => void }) => (
-  <div className='flex items-center border border-white p-2 rounded'>
-    <input type="number" placeholder="Number of bytes" value={value} onChange={onChange} className='mr-2 w-16 bg-transparent' />
-  </div>
-);
+const InputField = ({ array, onChange }: InputFieldProps) => {
+
+  return (
+    <>
+      <div className="font-bold mb-2">How long would you like your secret to be ?</div>
+      <div className='flex items-center border border-white p-2 rounded mb-2'>
+        <select name="bytes" id="bytes" defaultValue={256} onChange={onChange} className='text-center mr-2 bg-transparent focus:outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none'>
+              {array.map((v) => (
+                <option key={v} value={v}>{v}</option>
+              ))}
+        </select>
+        <p>bytes</p>
+      </div>
+    </>
+  )  
+}
 
 export default InputField;
