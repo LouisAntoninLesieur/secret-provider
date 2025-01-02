7d46148fcee247633cb32d435a4b51d2fa858b24
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-02 18:24:32 +0100

    feat: improve page

--- app/generate-secret/page.tsx
+++ app/generate-secret/page.tsx
@@ -4,8 +4,8 @@ import SecretForm from '../components/SecretForm';
 const GenerateSecretPage = () => {
   return (
     <>
-      <div className="text-6xl font-bold text-center w-1/3 mx-auto bg-clip-text text-transparent bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] from-neutral-600 via-green-50 to-green-50 m-4">
-        Generate a random secret, given a number of bytes
+      <div className="text-4xl font-bold text-center w-1/2 mx-auto mb-6">
+        Generate a JWT Secret
       </div>
       <SecretForm />
     </>
