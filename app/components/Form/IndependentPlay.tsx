d8ff15554079d1e05e07f75f412443753929d6fe
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-03 11:26:23 +0100

    fix: escape quotes

--- app/components/Form/IndependentPlay.tsx
+++ app/components/Form/IndependentPlay.tsx
@@ -6,7 +6,9 @@ const IndependentPlay = ({bytes}: IndependentPlayProps) => {
     <div className='font-bold m-2'>May you prefer to generate the secret yourself ? Open a terminal and run:</div>
     <div className='flex flex-col w-full sm:w-1/3 rounded mb-2'>
       {bytes && 
-        <div className="bg-slate-700 p-2 border border-white rounded break-words selection:bg-green-300 selection:text-black">node -e "console.log(require('crypto').randomBytes({bytes}).toString('hex'))"</div>
+        <div className="bg-slate-700 p-2 border border-white rounded break-words selection:bg-green-300 selection:text-black">
+          node -e "console.log(require('crypto').randomBytes({bytes}).toString('hex'))"
+        </div>
       }
     </div>
     </>
