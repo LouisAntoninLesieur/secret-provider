f5542306830cce94be553ba8bf096934f039c01a
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-03 15:11:42 +0100

    feat(aria): add a bunch of aria states

--- app/components/Form/IndependentDisplay.tsx
+++ app/components/Form/IndependentDisplay.tsx
@@ -6,7 +6,7 @@ const IndependentDisplay = ({bytes}: IndependentPlayProps) => {
     <div className='font-bold m-2'>May you prefer to generate the secret yourself ? Open a terminal and run:</div>
     <div className='flex flex-col w-full sm:w-1/3 rounded mb-2'>
       {bytes && 
-        <div className="bg-slate-700 p-2 border border-white rounded break-words selection:bg-green-300 selection:text-black">
+        <div className="bg-slate-700 p-2 border border-white rounded break-words selection:bg-green-300 selection:text-black" aria-label="Generate yourself a secret with node" title="Generate yourself a secret with node">
           node -e &quot;console.log(require(&apos;crypto&apos;).randomBytes({bytes}).toString(&apos;hex&apos;))&quot;
         </div>
       }
