faefbc9988a8ffb4b3c5c6bc390dd36c5529a0cb
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-07 19:02:20 +0100

    refactor: match components and function import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from "./components/Navbar";
+import { Navbar } from "../components/common/Navbar";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
