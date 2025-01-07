dcb45c09bea88432164fbbf0c9a4d80ef572d43c
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-07 18:46:23 +0100

    refactor: gather components in a new sub-folder to improve SoC

---
+++ components/common/Navbar.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex gap-10 text-2xl font-bold justify-start text-white bg-gray-200 bg-opacity-20 p-4 border rounded mx-10 my-10">
+      <Link href='/' aria-label="Go to Home" title="Go to Home">SecretProvider</Link>
+    </nav>
+  );
+}
