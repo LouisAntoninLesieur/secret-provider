58be6b94fe3d8553a108381e399a351b567ada94
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-09 08:14:57 +0100

    feat: clean repo from deleted files

--- app/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Navbar() {
-  return (
-    <nav className="flex gap-10 text-2xl font-bold justify-start text-white bg-gray-200 bg-opacity-20 p-4 border rounded mx-10 my-10">
-      <Link href='/' aria-label="Go to Home" title="Go to Home">SecretProvider.app</Link>
-    </nav>
-  );
-}
